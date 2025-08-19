c049b42e084f88a0667c1b164883cb2fcd203115
saliherdemk <saliherdem_kaymak@hotmail.com>
2025-08-19 15:01:55 +0300

    treesitter update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -7,7 +7,11 @@ return {
 
 			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup({
+<<<<<<< HEAD
 				ensure_installed = {"python", "lua"},
+=======
+				ensure_installed = { "python", "lua", "javascript" },
+>>>>>>> cba07b2 (treesitter update)
 				-- Autoinstall languages that are not installed
 				auto_install = true,
 				highlight = { enable = true },
